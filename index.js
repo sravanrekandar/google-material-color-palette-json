7e51e84959027be3d05f9e6f9e4e73b9383b12de
Sravan Kumar Rekandar <sravankumarrekandar@SRAVANs-MacBook-Pro.local>
2016-09-05 16:39:21 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+require('jsdom').env('', function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ 
+    var $ = require('jquery')(window);
+    require('./generateColorsJSON')($)
+});
